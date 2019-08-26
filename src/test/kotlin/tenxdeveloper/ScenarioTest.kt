1c2ac08f59497950af4ffcc5786f6d0369d65ef0
monkey codes <jz@monkey.codes>
2019-08-26 13:23:38 +1000

    Changed scenario test to use test input

--- src/test/kotlin/tenxdeveloper/ScenarioTest.kt
+++ src/test/kotlin/tenxdeveloper/ScenarioTest.kt
@@ -8,15 +8,10 @@ class ScenarioTest {
     @Test
     fun `it should rank the developer based on the given facts`(){
         val ranker = DeveloperRanker(
-            facts = EnglishFactParser().parse(
-                """|Jessie is not the best developer
-                   |Evan is not the worst developer
-                   |John is not the best developer or the worst developer
-                   |Sarah is a better developer than Evan
-                   |Matt is not directly below or above John as a developer
-                   |John is not directly below or above Evan as a developer""".trimMargin()
-            )
+            facts = EnglishFactParser().parse(testInput())
         )
         assertk.assert(ranker.rank()!!).matchesRanking("Sarah", "John", "Jessie", "Evan", "Matt")
     }
+
+    private fun testInput() = javaClass.getResourceAsStream("/input.txt").bufferedReader().readText()
 }
